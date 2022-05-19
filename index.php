cf6230e82e8ff9d811d7b1ddd78a6d3dfaaf5d79
tiktoins <103301694+tiktoins@users.noreply.github.com>
2022-05-19 17:08:07 +0430

    Update and rename TheScript.php to index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>
+Web Title
+</title>
+<style>
+body {
+    margin: 0;
+}
+</style>
+</head>
+<body>
+<iframe src="https://google.fingil.workers.dev/service/hvtrs8/-wuw,ymuvu%60e,cmm-" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+    Your browser doesn't support iframes
+</iframe>
+</body>
+</html>
